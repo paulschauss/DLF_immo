602c2912d2e0126a488f3e92d83494cab8c8364f
paul schauss <p+github@schauss.fr>
2022-09-29 17:59:59 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :mentions ]
 
   def home
     @houses = House.all.sort_by(&:state)
